f366de001ac04d33f97f43c04640983901fb94b4
olsondeven <olsondeven@gmail.com>
2016-11-01 12:04:56 -0600

    finished

---
+++ js/timeDirective.js
@@ -0,0 +1,14 @@
+angular.module('timeApp').directive('showTime', function(){
+  return {//scope{} this will limit your scope
+    restrict: 'E',
+    template: '<div>{{title}}<br/>Hello {{name}} the current time is {{time}}</div>',
+    scope:{
+      title: '@title'
+    },
+    link: function(scope, element, attrs){
+      scope.name = 'Deven'
+      var currentTime = new Date();//date()
+      scope.time = currentTime.toString();
+    }
+  }
+});
